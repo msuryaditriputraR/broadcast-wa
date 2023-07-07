4610b4cb04c891e532355aae1bda4cb630917478
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-07-07 13:34:08 +0800

    main app

--- index.js
+++ index.js
@@ -1,6 +1,8 @@
 const qrcode = require("qrcode-terminal");
-
 const { Client, LocalAuth } = require("whatsapp-web.js");
+const sendMessage = require("./utils/sendMessages");
+const data = require("./stored/data");
+const templateMessage = require("./utils/templateMessage");
 
 const client = new Client({
     authStrategy: new LocalAuth(),
@@ -12,6 +14,15 @@ client.on("qr", (qr) => {
 
 client.on("ready", () => {
     console.log("Client is ready!");
+
+    data.forEach((d) => {
+        const message = templateMessage({
+            name: d.name,
+            jalur: d.jalur,
+        });
+
+        sendMessage({ number: d.number, message, client });
+    });
 });
 
 client.initialize();
