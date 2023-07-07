8dbae34df72712edff7f621d7cddd625534cb6ea
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2023-07-07 13:33:58 +0800

    create template message utils

---
+++ utils/templateMessage.js
@@ -0,0 +1,16 @@
+function templateMessage({ name, jalur }) {
+    const link = "https://chat.whatsapp.com/BjhqZedSQPiIp1xOtu9KA1";
+
+    return `Assalamu 'alaikum Warahmatullahi Wabarakatuh, Selamat malam ${name} 
+
+  karena kamu lulus di Jurusan Teknik Komputer dan Jaringan di SMKN 4 GOWA melalui jalur ${jalur} 
+
+  Silahkan bergabung ke grup TKJ SMKN 4 GOWA 2023 melalui link berikut 
+
+  ${link}
+
+  *Jangan sebarkan link grup ini ke teman yang belum lulus, grup ini hanya untuk yang sudah lulus di TKJ!!
+  `;
+}
+
+module.exports = templateMessage;
