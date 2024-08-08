55d36b8171b28e63e9cd0d3fa7d23fd25bf0e57d
Muh Suryadi Triputra Rahman <msuryaditriputra@gmail.com>
2024-08-08 18:02:37 +0800

    import data from excel

--- stored/data.js
+++ stored/data.js
@@ -1,16 +1,7 @@
-const data = [
-    { name: "Faizah Nurul mentari", number: "6289522763215" },
-    { name: "Farida", number: "6282189625533" },
-    { name: "Nadia", number: "6288804203629" },
-    { name: "Misyaratul umma", number: "628875693710" },
-    { name: "Fadlan athoriq", number: "6287812172290" },
-    { name: "Husain", number: "62882020720967" },
-    { name: "Hana halifah", number: "6285758609617" },
-    { name: "Zulkifli", number: "62895605111760" },
-    { name: "Abd. Hakim", number: "62882021285882" },
-    { name: "Akmal", number: "6285825491600" },
-    { name: "Risky Al Fauzi", number: "6282348025280" },
-    { name: "Bayu Saputra", number: "6288804609428" },
-];
+const XLSX = require("xlsx");
+
+const workbook = XLSX.readFile("stored/data.xlsx");
+const worksheet = workbook.Sheets[workbook.SheetNames[0]];
+const data = XLSX.utils.sheet_to_json(worksheet);
 
 module.exports = data;
